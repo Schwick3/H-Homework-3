887aa4c22eaf2a895f7be0a45b91296fd7c9c185
kcgbl <kcgbl@the-machine>
2024-02-16 18:10:22 -0600

    Added constructor 2 and getters/setters

--- src/edu/mu/inventory/MediaProduct.java
+++ src/edu/mu/inventory/MediaProduct.java
@@ -20,6 +20,31 @@ public class MediaProduct {
 		this.year = Integer.parseInt(year);
 		this.genre = Genre.valueOf(genre);
 	}
+	//constructor 2
+		public MediaProduct(String title2, double price2, int year2, Genre genre2) {
+			this.title = title2;
+			this.price = price2;
+			this.year = year2;
+			this.genre = genre2;
+		}
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public int getYear() {
+			return year;
+		}
+		public void setYear(int year) {
+			this.year = year;
+		}
+		public Genre getGenre() {
+			return genre;
+		}
+		public void setGenre(Genre genre) {
+			this.genre = genre;
+		}
 	@Override
 	public String toString() {
 		return "Title = " + title + ", Price = " + price + ", Year = " + year + ", Genre = " + genre + "]";
